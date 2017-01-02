0aea5ff5fa80ba5d86257eee5b54223dc9606a80
Tan Duong <dntan09@gmail.com>
2017-01-02 22:48:28 +0700

    Attempt to fix heroku migration

--- db/migrate/20170102114144_add_lonlat_to_driver.rb
+++ db/migrate/20170102114144_add_lonlat_to_driver.rb
@@ -1,5 +1,6 @@
 class AddLonlatToDriver < ActiveRecord::Migration[5.0]
   def change
+    enable_extension :postgis
     add_column :drivers, :lonlat, :st_point, geographic: true, srid: 4326
     add_index :drivers, :lonlat, using: :gist
   end
